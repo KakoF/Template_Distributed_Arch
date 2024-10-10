2774c27f13101c95aa183c1a4be1ede4ea14a5b5
Kako Ferrare <kakoferrare87@gmail.com>
2024-10-10 14:19:41 -0300

    remove project

--- DistributedArch/API-EntityFramework/ApiConfiguration.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace API_EntityFramework
-{
-	public static class ApiConfiguration
-	{
-		public static string ConnectionString { get; set; } = string.Empty;
-	}
-}
--- DistributedArch/API-EntityFramework/k6/README.md
+++
@@ -1,6 +0,0 @@
-Env:
-Windows:
-choco install k6
-
-Exec:
-k6 run script.js
