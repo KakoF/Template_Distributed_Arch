74d97e47b3656961a3e45cc7c83f7c704ea41c20
Kako Ferrare <kakoferrare87@gmail.com>
2024-10-11 19:15:03 -0300

    docs: grafana dash export

--- DistributedArch/API-EntityFramework/k6/getall.js
+++ DistributedArch/API-EntityFramework/k6/getall.js
@@ -7,9 +7,9 @@ export let options = {
     stages: [
         { duration: "30s", target: 100 }, // Sobe para 100 usuários em 30 segundos
         { duration: "1m", target: 200 }, // Mantém 200 usuários por 1 minuto
-        { duration: "1m", target: 500 }, // Sobe para 500 usuários em 1 minuto
+        /*{ duration: "1m", target: 500 }, // Sobe para 500 usuários em 1 minuto
         { duration: "30s", target: 1000 }, // Sobe para 1000 usuários em 30 segundos
-        { duration: "30s", target: 0 }, // Reduz para 0 usuários (finaliza o teste)
+        { duration: "30s", target: 0 }, // Reduz para 0 usuários (finaliza o teste)*/
     ],
     thresholds: {
         http_req_duration: ["p(95)<500"], // 95% das requisições devem ter menos de 500ms de duração
