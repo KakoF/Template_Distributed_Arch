3f951d12108ab32deda5561510160503b095fe04
Kako Ferrare <kakoferrare87@gmail.com>
2025-07-22 14:24:07 -0300

    feat: added redis pubsub example

---
+++ DistributedArch/RedisPubSub/RedisPublisher/Program.cs
@@ -0,0 +1,11 @@
+﻿using StackExchange.Redis;
+
+var redis = await ConnectionMultiplexer.ConnectAsync("localhost:6379,password=Admin@#,ConnectTimeout=10000");
+var pub = redis.GetSubscriber();
+
+for (int i = 1; i <= 5; i++)
+{
+	await pub.PublishAsync("canal-mensagens", $"Mensagem {i}");
+	Console.WriteLine($"Mensagem {i} publicada.");
+	await Task.Delay(1000);
+}
---
+++ DistributedArch/RedisPubSub/RedisSubscriber/Program.cs
@@ -0,0 +1,12 @@
+﻿using StackExchange.Redis;
+
+var redis = ConnectionMultiplexer.Connect("localhost:6379,password=Admin@#,ConnectTimeout=10000");
+var sub = redis.GetSubscriber();
+
+sub.Subscribe("canal-mensagens", (channel, message) =>
+{
+	Console.WriteLine($"Recebido: {message}");
+});
+
+Console.WriteLine("Assinando canal... Pressione qualquer tecla para sair.");
+Console.ReadKey();
